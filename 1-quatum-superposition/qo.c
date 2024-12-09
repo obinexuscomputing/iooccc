24a866dc3478c0c466605374b82886bd27400a4e
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-09 04:12:33 +0000

    init

--- 1-quatum-superposition/qo.c
+++ 1-quatum-superposition/qo.c
@@ -10,7 +10,7 @@
 #define W while
 #define PI M_PI
 #define _(x) return x
-#define $(x) ((x)*PI/180.)
+#define D(x) ((x)*PI/180.0)
 typedef double d;
 typedef double complex c;
 struct q{int s;c a;d p,h;}_;
@@ -21,11 +21,11 @@ void e(struct q*t,d*h,int n){
     z[i]=.25+.15*sin((n+i)*u)*cos((n+i)*k);
     z[i]+=0.1*((d)R/100-.5);
     t[i].p=m(z[i],.05);v+=t[i].p;}F(4){
-    int j=(i+1)%4;d f=cos(fabs(t[i].h-t[j].h)*$/180)*.1;
+    int j=(i+1)%4;d f=cos(fabs(t[i].h-t[j].h)*D(1))*.1;
     t[i].p+=f;t[j].p-=f;}d s=0;F(4)s+=t[i].p;
     F(4){t[i].p/=s;d r=60+20*sin(n*PI/3)+(R%20)-10;
     t[i].h+=r;w(&t[i].h,0,360);
-    t[i].a=sqrt(t[i].p)*cexp(_Complex_I*$(t[i].h));}}
+    t[i].a=sqrt(t[i].p)*cexp(_Complex_I*D(t[i].h));}}
 void v(struct q*t){P("\nState Distribution:\n");d x=0;
 F(4)x=m(x,t[i].p);F(4){P("  |%d%d> ",(t[i].s&2)>>1,t[i].s&1);
 int l=40*t[i].p/x;F(l)P("%s",G(i,l/4,b));
